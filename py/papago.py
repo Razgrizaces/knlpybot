359d2999ba0af40d3353a49675ee2113bd85140e
Tristen Terracciano <tristen.terracciano@gmail.com>
2019-01-31 17:40:16 -0500

    took some of the keys out, still need to take the rest out and fix the usage

--- py/papago.py
+++ py/papago.py
@@ -1,10 +1,37 @@
 import os
 import sys
 import urllib.request
-client_id = "cNG69MHtiwvlRqbBHSJK"
-client_secret = "tTeMWZEjRT"
+import json
+
+with open("..\\..\\tokens.json") as f:
+	data = json.load(f)
+
+client_id =  data["tokens"]["papago-id"]
+client_secret = data["tokens"]["papago-token"]
 encText = urllib.parse.quote("Hi, I'm Tris.")
-data = "source=" + "&target="_+ "&text=" + encText
+inLang = -1
+outLang = -1
+while not (inLang == 0 or inLang ==1):
+	try:	
+		inLang = int(input("Input data: type 0 for 한국어, type 1 for English.\n"))
+	except ValueError:
+		print("Please type either 1 or 0.\n")
+		
+while not (outLang == 0 or outLang == 1):
+	try:	
+		outLang = int(input("Output data: type 0 for 한국어, type 1 for English.\n"))
+	except ValueError:
+		print("Please type either 1 or 0.\n")
+if (inLang == 0):
+	inLang = "ko"
+else:
+	inLang = "en"
+if (outLang == 0):
+	outLang = "ko"
+else:
+	outLang = "en"
+data = "source=" + inLang + "&target=_"+ outLang + "&text=" + encText
+print(data)
 url = "https://openapi.naver.com/v1/papago/n2mt"
 request = urllib.request.Request(url)
 request.add_header("X-Naver-Client-Id",client_id)
