d6a7c324546572aeabd09817d7f88097262e2864
Tristen Terracciano <tristen.terracciano@gmail.com>
2019-02-05 16:20:21 -0500

    Added papago functionality, working on konlpy functionality...

--- py/jsontest.py
+++
@@ -1,4 +0,0 @@
-{"message":{"@type":"response","@service":"naverservice.nmt.proxy","@version":"1
-.0.0","result":{"srcLangType":"ko","tarLangType":"en","translatedText":"Currentl
-y, there is a publishing contract with another publishing company (music publish
-ing company) for all songs or songs in Korea or overseas or overseas."}}}
--- py/knlpbot.py
+++ py/knlpbot.py
@@ -13,19 +13,22 @@ with open("..\\..\\tokens.json") as f:
 	f.close()
 
 TOKEN = tokens["tokens"]["bot-token"]
-client = discord.Client()
 
 bot = commands.Bot(command_prefix="!")
-
-@client.event
-async def papagoRequest(message, client, args):
+client = discord.Client()
+@bot.command(pass_context=True)
+async def translate(ctx):
+	'''
+	!translate inLang outLang message | languages supported: en(glish) ko(rean) zn-CH zn-TW ja(panese) es(panol) fr(ench) vi(etnamiese) th(ai) id(indonesian) ge(rman)
+	'''
+	dataIn = ctx.message.content
 	client_id =  tokens["tokens"]["papago-id"]
 	client_secret = tokens["tokens"]["papago-token"]
 	languages = {'en', 'ko', 'zn-CH', 'zn-TW', 'ja', 'es', 'fr', 'vi', 'th', 'id', 'ge'}
-	dataIn = message.content()
 	splitData = dataIn.split(" ")
 	if splitData[1] not in languages and splitData[2] not in languages:
-		return "Incorrect input or output language.".format(message)
+		await client.send_message(ctx.message.channel, "Incorrect input or output language.")
+		return
 	inLang = splitData[1]
 	outLang = splitData[2]
 	#get message part
@@ -36,65 +39,48 @@ async def papagoRequest(message, client, args):
 	request = urllib.request.Request(url)
 	request.add_header("X-Naver-Client-Id",client_id)
 	request.add_header("X-Naver-Client-Secret",client_secret)
-	response = urllib.request.urlopen(request, data=data.encode("utf-8"))
-	rescode = response.getcode()
-	if(rescode==200):
-		response_body = response.read()
-		responseJson = json.loads(response_body.decode('utf-8'))
-		responseMsg = responseJson['message']['result']['translatedText'].format(message)
-		return responseMsg
-	else:
-		return 'Papago Request Failed.'.format(message)
+	try:
+		response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+		rescode = response.getcode()
+		if(rescode==200):
+			response_body = response.read()
+			responseJson = json.loads(response_body.decode('utf-8'))
+			responseMsg = ctx.message.author.mention + " your translated message: "
+			responseMsg += responseJson['message']['result']['translatedText']
+			await client.send_message(ctx.message.channel, responseMsg)
+		else:
+			await client.send_message(ctx.message.channel, ctx.message.author.mention + ' Papago Request Failed.')
+	except:
+		await client.send_message(ctx.message.channel, ctx.message.author.mention + ' Papago Request Failed.')
 
-@client.event
-async def test(message):
-	msg = 'Hello {0.author.mention}'.format(message)
-	await client.send_message(message.channel, msg)
+@bot.command(pass_context=True)
+async def test(ctx):
+	msg = 'Hello {0.author.mention}'
+	await client.send_message(ctx.message.channel, msg)
 
-@client.event
-async def konlpy(message, client, args):
-	msg = 'konlpy test **콜엔피우아이** {0.author.mention}'.format(message)
-	await client.send_message(message.channel, msg)
-ch.add_command({
-	'trigger': "!konlpy",
-	'function': konlpy,
-	'args_num': 0, #need at least one thing to translate
-	'args_name': [],
-	'description': "starts the nlp processor for konlpy"
-})	
-@client.event
-async def kill(message, client, args):
+@bot.command(pass_context=True)
+async def konlpy(ctx):
+	msg = 'konlpy test **콜엔피우아이** ' + ctx.message.author.mention
+	await client.send_message(ctx.message.channel, msg)
+
+@bot.command(pass_context=True)
+async def kill(ctx):
+	await client.send_message(ctx.message.channel, "Kill me")
 	await client.logout()
-	return "Kill me".format(message)
-ch.add_command({
-	'trigger': "!kill",
-	'function': kill,
-	'args_num': 0, #need at least one thing to translate
-	'args_name': [],
-	'description': "this kills the bot"
-})	
+
+@bot.command(pass_context=True)
+async def commands(ctx):
+	msg =  "**List of Commands: \n!translate inLang outLang message**\n*languages: en(glish), ko(rean), zn-CH(inese-Standard), zn-TW(Taiwanese), ja(panese), es(panol), fr(ench), vi(etnamese, id(indonesian), th(ai), ge(rman)*\nTranslates from inLang to outLang using Papago.\n\n"
+	msg += "**konlpy verbosity sentence**\n*verbosity = how detailed you want the response to be* \nkonlpy is a NLP that attemps to properly extract the grammar of a sentence. May not completely be accurate.\n"
+	await client.send_message(ctx.message.channel, msg)
 #main method of bot
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-	if message.author == client.user:
-		return
-	else:
-		# try to evaluate with the command handler
-		try:
-			bot.process_commands(message)
-		# message doesn't contain a command trigger
-		except TypeError as e:
-			pass
-		# generic python error
-		except Exception as e:
-			print(e)
-			
+	print("The message's content was", message.content)
+	await bot.process_commands(message)
+
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print('Logged in as your bot xd')
 
 client.run(TOKEN)
