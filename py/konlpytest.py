1cef657802138268b5566218dfae880332fffa1d
Tristen Terracciano <tristen.terracciano@gmail.com>
2019-02-08 17:09:22 -0500

    Added twitter nlp functionalty, gotta make it a little more seamless now...

--- py/konlpytest.py
+++ py/konlpytest.py
@@ -1,5 +1,5 @@
+from konlpy.tag import Okt
 from konlpy.tag import Kkma
-from konlpy.tag import Mecab
 from konlpy.utils import pprint
 import tossi 		
 import io		
@@ -19,36 +19,53 @@ import pandas as pd
 
 print("Loading KKMA...")
 kkma=Kkma()
+okt=Okt()
 print("Done loading!")
+#invalidTags = {"SF", "SE", "SS", "SP", "SO", "SW"}
+sentence = '안녀하세요, 저는 트리스예요 ㅋㅋㅋㅋㅋ 저는 뉴욕에 살아요. 만나서 반사습니다~'
 filename = "..\\csvs\\konlpytest.csv"
-sentence = '발표 무사히 끝냈어요! 주말에 빈 회의실 가서 혼자 연습했어요 ㅠㅠ 청중역으로 인형 갖다두고 그거 보면서 연습하고 발표할때도 저건 사람이 아니라 인형이다 이러고 했습니다 ㅠㅠ'
-invalidTags = {"SF", "SE", "SS", "SP", "SO", "SW"}
+invalidTags = {"Unknown", "Number","Foreign", "Alpha", "Hashtag","ScreenName", "Email", "URL", "Punctuation"}
 i = 0
 #create the csv
 with io.open(filename, "w", encoding = "utf8") as f:
-	while i is not len(kkma.sentences(sentence)):
+	#while i is not len(kkma.sentences(sentence)):
+	#	#tuple check
+	#	j = 0
+	#	while j is not len(kkma.pos(kkma.sentences(sentence)[i])):
+	#		if kkma.pos(kkma.sentences(sentence)[i])[j][1] not in invalidTags:
+	#			f.write(kkma.pos(kkma.sentences(sentence)[i])[j][0])
+	#			f.write(",")
+	#			f.write(kkma.pos(kkma.sentences(sentence)[i])[j][1])
+	#			f.write("\n")
+	#		j = j+1
+	#	i = i + 1
+	#while i is not len(okt.sentences(sentence)):
 		#tuple check
-		j = 0
-		while j is not len(kkma.pos(kkma.sentences(sentence)[i])):
-			if kkma.pos(kkma.sentences(sentence)[i])[j][1] not in invalidTags:
-				f.write(kkma.pos(kkma.sentences(sentence)[i])[j][0])
-				f.write(",")
-				f.write(kkma.pos(kkma.sentences(sentence)[i])[j][1])
-				f.write("\n")
-			j = j+1
-		i = i + 1
+		#j = 0
+	while i is not len(okt.pos(sentence)):
+		if okt.pos(sentence)[i][0] not in invalidTags:
+			f.write(okt.pos(sentence)[i][0])
+			f.write(",")
+			f.write(okt.pos(sentence)[i][1])
+			f.write("\n")
+		i = i + 1	
 	f.close()
 #csv created
 
-cols = ["pos","korean","T1 Class KR","T1 Class English","T3 Korean","T3 English","sentence examples"]
-posCols = ["word", "pos"]
+#cols = ["pos","korean","T1 Class KR","T1 Class English","T3 Korean","T3 English","sentence examples"]
+cols = ["pos","korean1"]
+posCols = ["pos", "korean"]
 
 #create data frame from cols with nlp_to_level
-nlpDf = pd.read_csv("..\\csvs\\nlp_to_level.csv", names = cols)
+#nlpDf = pd.read_csv("..\\csvs\\nlp_to_level.csv", names = cols)
+nlpDf = pd.read_csv("..\\csvs\\twt_to_level.csv", names = cols)
 #create df using posCols with file from before
 posDf = pd.read_csv(filename, names= posCols)
 
 #merge the dfs
 print(nlpDf)
-posDf = posDf.merge(nlpDf, on = ["pos"])
+print(posDf)
 posDf.to_csv("testdf.csv")
+posDf = posDf.merge(nlpDf, on = ["pos"])
+print(posDf)
+#posDf.to_csv("testdf.csv")
